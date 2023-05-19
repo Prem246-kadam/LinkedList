80e436e69aa383ef1210e245aabf69a9d48620a6
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-05-19 12:23:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
+using System;
+using Collection.Generic;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
 
+        // Adding nodes to the linked list
+        list.AddNode(56);
+        list.AddNode(30);
+        list.AddNode(70);
+         list.AddNode(80);
+
+        // Displaying the linked list
+        list.DisplayList();
+     
+
+     list.RemoveFirstNode();
+        list.DisplayList();
+list.length();
+        Console.WriteLine();
+    }
+}
